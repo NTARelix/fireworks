0c870abf82efcb24b33ddc68136c0b60d2c45831
Kevin Koshiol <ntarelix@gmail.com>
2020-04-13 23:45:56 -0500

    refactor: change rect to grey rocket

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import { createProgram } from './graphics/create-program.js'
 import { createShader } from './graphics/create-shader.js'
-import fragmentShaderSource from './graphics/shader-fragment.js'
-import vertexShaderSource from './graphics/shader-vertex.js'
 import { createMatrix, projection } from './graphics/matrix3.js'
+import rocketFragmentShaderSource from './graphics/rocket-shader-fragment.js'
+import rocketVertexShaderSource from './graphics/rocket-shader-vertex.js'
 
 function main() {
   /** @type {HTMLCanvasElement} */
@@ -11,8 +11,8 @@ function main() {
   canvas.height = document.body.clientHeight
   const gl = canvas.getContext('webgl2')
   if (!gl) return alert('Failed to create WebGL2 context. It is likely that you are running an older browser.')
-  const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource)
-  const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource)
+  const vertexShader = createShader(gl, gl.VERTEX_SHADER, rocketVertexShaderSource)
+  const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, rocketFragmentShaderSource)
   const program = createProgram(gl, vertexShader, fragmentShader)
   const positionAttributeLocation = gl.getAttribLocation(program, 'a_position')
   const projectionMatrixUniformLocation = gl.getUniformLocation(program, 'u_projection_matrix')
@@ -21,13 +21,9 @@ function main() {
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
   const positions = new Float32Array([
     // Top-left triangle
-    10, 20,
-    80, 20,
-    10, 30,
-    // Bottom-right triangle
-    10, 30,
-    80, 20,
-    80, 30,
+    10, 15,
+    13, 30,
+    7, 30,
   ])
   gl.bufferData(gl.ARRAY_BUFFER, positions, gl.STATIC_DRAW)
   const vao = gl.createVertexArray()
@@ -37,13 +33,13 @@ function main() {
   const projectionMatrix = createMatrix()
   projection(projectionMatrix, gl.canvas.width, gl.canvas.height)
   gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
-  gl.clearColor(0, 0, 0, 0)
+  gl.clearColor(0, 0, 0, 1)
   gl.clear(gl.COLOR_BUFFER_BIT)
   gl.useProgram(program)
   gl.bindVertexArray(vao)
   gl.uniformMatrix3fv(projectionMatrixUniformLocation, false, projectionMatrix)
-  gl.uniform4f(colorUniformLocation, Math.random(), Math.random(), Math.random(), 1)
-  gl.drawArrays(gl.TRIANGLES, 0, 6)
+  gl.uniform4f(colorUniformLocation, 0.2, 0.2, 0.2, 1)
+  gl.drawArrays(gl.TRIANGLES, 0, 3)
 }
 
 main()
