fb02be9c99bedda5f52c4394d880a91413e6bd18
Kevin Koshiol <ntarelix@gmail.com>
2020-04-14 20:41:25 -0500

    feat: add rockets with mouse

--- index.js
+++ index.js
@@ -6,16 +6,18 @@ function main() {
   canvas.width = document.body.clientWidth
   canvas.height = document.body.clientHeight
   const render = createRenderer(canvas)
-  const rockets = [
-    { x: 750, y: 500 },
-    { x: 100, y: 200 },
-    { x: 1000, y: 20 },
-    { x: 1500, y: 800 },
-  ]
+  const rockets = []
   render(rockets)
   addEventListener('resize', () => {
     canvas.width = document.body.clientWidth
     canvas.height = document.body.clientHeight
+    render(rockets)
+  })
+  addEventListener('click', event => {
+    rockets.push({
+      x: event.clientX,
+      y: event.clientY,
+    })
     render(rockets)
   })
 }
