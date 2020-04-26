efc4be729b3552de5a2e7a96bdf50d93a5b147be
Kevin Koshiol <koshiol.kevin@gmail.com>
2020-04-25 20:28:14 -0500

    refactor(MATRIX): improve usability
    - return array from each utility
    - docs refer to more appropriate storage type

--- graphics/matrix3.js
+++ graphics/matrix3.js
@@ -11,7 +11,7 @@ const M22 = 2 * 3 + 2
 
 /**
  * Utility for more concise array setting
- * @param {ArrayBuffer} array Array in which the matrix data will be set
+ * @param {Float32Array} outArray Array in which the matrix data will be set
  * @param {number} m00 Value at [0,0]
  * @param {number} m10 Value at [1,0]
  * @param {number} m20 Value at [2,0]
@@ -21,36 +21,38 @@ const M22 = 2 * 3 + 2
  * @param {number} m02 Value at [0,2]
  * @param {number} m12 Value at [1,2]
  * @param {number} m22 Value at [2,2]
+ * @returns {Float32Array}
  */
-function setArray(array, m00, m10, m20, m01, m11, m21, m02, m12, m22) {
-  array[M00] = m00
-  array[M10] = m10
-  array[M20] = m20
-  array[M01] = m01
-  array[M11] = m11
-  array[M21] = m21
-  array[M02] = m02
-  array[M12] = m12
-  array[M22] = m22
+function setArray(outArray, m00, m10, m20, m01, m11, m21, m02, m12, m22) {
+  outArray[M00] = m00
+  outArray[M10] = m10
+  outArray[M20] = m20
+  outArray[M01] = m01
+  outArray[M11] = m11
+  outArray[M21] = m21
+  outArray[M02] = m02
+  outArray[M12] = m12
+  outArray[M22] = m22
+  return outArray
 }
 
 /**
  * Creates a new 3x3 identity matrix
- * @returns {ArrayBuffer} New array
+ * @returns {Float32Array} New array
  */
 export function createMatrix() {
   const matrix = new Float32Array(3 * 3)
-  identity(matrix)
-  return matrix
+  return identity(matrix)
 }
 
 /**
  * Creates an identity matrix for 2d space
- * @param {ArrayBuffer} array Array in which the identity matrix will be written
+ * @param {Float32Array} outArray Array in which the identity matrix will be written
+ * @returns {Float32Array}
  */
-export function identity(array) {
-  setArray(
-    array,
+export function identity(outArray) {
+  return setArray(
+    outArray,
     1, 0, 0,
     0, 1, 0,
     0, 0, 1,
@@ -61,13 +63,14 @@ export function identity(array) {
  * Creates a transformation matrix in for vertex shader projection in 2d space
  * from 2d coordinate space (0 to resolution; left to right and top to bottom)
  * to 2d clip space (-1 to 1; left to right and bottom to top)
- * @param {ArrayBuffer} array Array in which the projection matrix will be written
+ * @param {Float32Array} outArray Array in which the projection matrix will be written
  * @param {number} width Resolution width
  * @param {number} height Resolution height
+ * @returns {Float32Array}
  */
-export function projection(array, width, height) {
-  setArray(
-    array,
+export function projection(outArray, width, height) {
+  return setArray(
+    outArray,
     2 / width, 0, 0,
     0, -2 / height, 0,
     -1, 1, 1,
@@ -76,13 +79,14 @@ export function projection(array, width, height) {
 
 /**
  * Creates a translation matrix for 2d space
- * @param {ArrayBuffer} array Array in which the translation matrix will be written
+ * @param {Float32Array} outArray Array in which the translation matrix will be written
  * @param {number} tx X translation quantity
  * @param {number} ty Y translation quantity
+ * @returns {Float32Array}
  */
-export function translation(array, tx, ty) {
-  setArray(
-    array,
+export function translation(outArray, tx, ty) {
+  return setArray(
+    outArray,
     1, 0, 0,
     0, 1, 0,
     tx, ty, 1,
@@ -91,14 +95,15 @@ export function translation(array, tx, ty) {
 
 /**
  * Creates a rotation matrix for 2d space
- * @param {ArrayBuffer} array Array in which the rotation matrix will be written
+ * @param {Float32Array} outArray Array in which the rotation matrix will be written
  * @param {number} angle Rotation angle (radians)
+ * @returns {Float32Array}
  */
-export function rotation(array, angle) {
+export function rotation(outArray, angle) {
   const dx = Math.cos(angle)
   const dy = Math.sin(angle)
-  setArray(
-    array,
+  return setArray(
+    outArray,
     dx, -dy, 0,
     dy, dx, 0,
     0, 0, 1,
@@ -107,13 +112,14 @@ export function rotation(array, angle) {
 
 /**
  * Creates a scaling matrix for 2d space
- * @param {ArrayBuffer} array Array in which the scaling matrix will be written
+ * @param {Float32Array} outArray Array in which the scaling matrix will be written
  * @param {number} sx X scale factor
  * @param {number} sy Y scale factor
+ * @returns {Float32Array}
  */
-export function scaling(array, sx, sy) {
-  setArray(
-    array,
+export function scaling(outArray, sx, sy) {
+  return setArray(
+    outArray,
     sx, 0, 0,
     0, sy, 0,
     0, 0, 1,
@@ -122,13 +128,14 @@ export function scaling(array, sx, sy) {
 
 /**
  * Multiplies 2 arrays a*b
- * @param {ArrayBuffer} array Array in which the resulting array multiplication will be written
- * @param {ArrayBuffer} a Left matrix
- * @param {ArrayBuffer} b Right matrix
+ * @param {Float32Array} outArray Array in which the resulting array multiplication will be written
+ * @param {Float32Array} a Left matrix
+ * @param {Float32Array} b Right matrix
+ * @returns {Float32Array}
  */
-export function multiply(array, a, b) {
-  setArray(
-    array,
+export function multiply(outArray, a, b) {
+  return setArray(
+    outArray,
     b[M00] * a[M00] + b[M01] * a[M10] + b[M02] * a[M20],
     b[M00] * a[M01] + b[M01] * a[M11] + b[M02] * a[M21],
     b[M00] * a[M02] + b[M01] * a[M12] + b[M02] * a[M22],
