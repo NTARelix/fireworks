ecbbfd8e3b758a2d014fc552a25d175c4ab2732d
Kevin Koshiol <ntarelix@gmail.com>
2020-05-07 10:15:43 -0500

    refactor(SPARK): simple spark uses randomization utilities

--- spark-factories/simple.js
+++ spark-factories/simple.js
@@ -1,5 +1,6 @@
 import { COLORS } from '../entities/color.js'
 import { Spark } from '../entities/spark.js'
+import { randomChoice, randomRange } from '../math/random.js'
 
 const COLOR_CHOICES = [
   COLORS.RED,
@@ -18,18 +19,14 @@ const COLOR_CHOICES = [
  * @param {number} time
  */
 export function createSimpleExplosion(sparks, x, y, time) {
-  const newSparkCount = 650 + Math.random() * 100
-  const fireworkPower = 50 + Math.random() * 200
-  const sparkColor = COLOR_CHOICES[Math.floor(Math.random() * COLOR_CHOICES.length)]
+  const newSparkCount = randomRange(650, 750)
+  const fireworkPower = randomRange(50, 250)
+  const sparkColor = randomChoice(COLOR_CHOICES)
   for (let s = 0; s < newSparkCount; s++) {
-    sparks.push(new Spark(
-      x,
-      y,
-      Math.random() * 2 * Math.PI,
-      10 + Math.random() * fireworkPower,
-      700 + Math.random() * 300,
-      time,
-      sparkColor,
-    ))
+    const direction = randomRange(0, 2 * Math.PI)
+    const distance = randomRange(10, 10 + fireworkPower)
+    const duration = randomRange(700, 1000)
+    const spark = new Spark(x, y, direction, distance, duration, time, sparkColor)
+    sparks.push(spark)
   }
 }
